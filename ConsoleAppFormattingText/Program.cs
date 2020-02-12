906ee80d087269e13fc972fba3f0d7a963e38431
ukgarethevans@gmail.com <ukgarethevans@gmail.com>
2020-02-12 11:40:58 +0000

    initial upload

---
+++ ConsoleAppFormattingText/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+/* **********************************************************************************************************************
+ * Location: C:\Users\EvansG\source\repos\Udemy_Course
+ * 
+ * Make sure that you set the Console App as a Startup Project.
+ * Just right click on your project and select Set As Startup Project. 
+ * Your project name will become bold
+ * This is critical for a Console App project that runs with 
+ * F5 keyboard shortcut so that the compiler knows where to enter the program. 
+ *
+*************************************************************************************************************************/
+
+namespace ConsoleAppFormattingText
+{
+    class AppFormatting
+    {
+        static void Main(string[] args)
+        {
+            double pricePerOunce = 17.36;
+            string outputString, headers, outputStringOne;
+            string itemName = "Widget";
+
+            //format output
+            outputString = String.Format("The current price is {0:C}", pricePerOunce);
+
+            headers = String.Format("{0,10} {1,10}", "Item name", "Item Cost");
+            outputStringOne = String.Format("{0,10} {1,10}", itemName, pricePerOunce);
+
+            //output
+            Console.WriteLine(outputString);
+
+            Console.WriteLine(headers);
+            Console.WriteLine(outputStringOne);
+
+
+            // escaping chars
+            // the @ sign makes the string a literal
+            string file = @"C:\Automation\Results";
+            // if you want a carriage return though then us the following
+            string file1 = "This is text on one line \nand this is on the next";
+            string file2 = "C:\\Autom\nation\\Results";
+            Console.WriteLine(file);
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine(file1);
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine(file2);
+            Console.WriteLine("------------------------------------------------");
+        }
+    }
+}
---
+++ Section4_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Section4_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section4_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51e3aa99-8807-4ab2-a61e-857070c472dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
