dd816f92b9314c84a81be050d7a544551788fb24
ukgarethevans@gmail.com <ukgarethevans@gmail.com>
2020-02-13 09:22:52 +0000

    Created Tests2 for the quiz and amended the name of the first class to Attributes_Assertions_and_exceptions as well as adding exceptions code to that class.

---
+++ Section4_UnitTest/Attributes_Assertions_and_exceptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/*
+ Location: C:\Users\EvansG\source\repos\Udemy_Course (work)
+
+    Lesson 45 Assertions
+    Lesson 46 Exceptions
+
+    [ExpectedException] is almost never used in functional test automation. I actually can't think of a time that I used it.
+    This attribute is very common in unit testing, when you would expect some method to throw some exception.
+ */
+
+namespace Section4_UnitTest
+{
+    [TestClass]
+    [TestCategory("LoginFeature")]
+
+    public class Attributes_Assertions_and_exceptions
+    {
+        [TestMethod]
+        [TestCategory("positive")]
+        [Description("DemoTest")]
+        public void TestMethod1()
+        {
+            var a = 1;
+            var b = 2;
+         Assert.AreEqual(3,a+b);  // better as it gives a better error msg. Shows what was expected and what was returned.
+         Assert.AreNotEqual(a,4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException ))]    // so now with this exception the test will fail if the excemtopn in NOT thrown
+        public void TestMethod2()
+        {
+            var a = 1;
+            Assert.IsTrue(a == 1);  //doesnt give a very useful error msg, just that it fails.
+            Assert.AreNotEqual(1, a);   // This fails, but the test fasses becasue of the exception handler
+        }
+    }
+}
---
+++ Section4_UnitTest/Tests2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section4_UnitTest
+{
+    [TestClass]
+    [TestCategory("Quiz")]
+    public class Tests2
+    {
+        [TestMethod]
+        public void test1()
+        {
+            var a = 1;
+            var b = 1;
+            Assert.AreEqual(2, a + b);
+
+        }
+        [TestMethod]
+        public void test2()
+        {
+            var x = 1;
+            var y = 2;
+            Assert.AreEqual(3, x + y);
+            Assert.Fail();     // forces a failure
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void test3()
+        {
+            int a = 10;
+            int b = 10;
+            Assert.AreEqual(21, a + b);
+        }
+    }
+}
