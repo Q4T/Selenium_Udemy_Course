849cc28bc31c1fe0fc43614344017165e3e3b087
Gareth <Gareth@WindowsHome>
2020-02-20 07:06:42 +0000

    Lesson 54 Math with integers

---
+++ Section_5_Maths_Equality_and Rational_Operators/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Section_5_Maths_Equality_and Rational_Operators")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section_5_Maths_Equality_and Rational_Operators")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c254e73-2e2b-4ca2-b826-942f5c0f78d0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Section_5_Maths_Equality_and Rational_Operators/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section_5_Maths_Equality_and_Rational_Operators
+{
+
+    /*****************************************************************
+     * 
+     * Lesson 54 Math with integers
+     * 
+     * 
+     * 
+     * *****************************************************************/
+    [TestClass]
+    public class IntegerOperators
+    {
+        // variables
+        static int number1;
+        static int number2;
+
+        [ClassInitialize()]
+        public static void IntegerInitialise(TestContext testContext)
+        {
+            number1 = 10;
+            number2 = 5;
+
+        }
+
+
+        [TestMethod]
+        public void Test_Addition()
+        {
+            int sum = number1 + number2;
+            Console.WriteLine(sum);
+            Assert.AreEqual(sum, 15);
+        }
+
+        [TestMethod]
+        public void Test_Subtraction()
+        {
+            int difference = number1 - number2;
+            Console.WriteLine(difference);
+            Assert.AreEqual(difference, 5);
+        }
+
+        [TestMethod]
+        public void Test_Multiplication()
+        {
+            int times = number1 * number2;
+            Console.WriteLine(times);
+            Assert.AreEqual(times, 50);
+        }
+
+        [TestMethod]
+        public void Test_Division()
+        {
+            int divide = number1 / number2;
+            Console.WriteLine(divide);
+            Assert.AreEqual(divide, 2);
+        }
+
+        [TestMethod]
+        public void Test_Modulus()
+        {
+            int remainder = number1 % number2;
+            Console.WriteLine(remainder);
+            Assert.AreEqual(remainder, 0);
+        }
+
+    }
+}
