dbc136ebf8665c4b85a5575892f440b9712187c5
ukgarethevans@gmail.com <ukgarethevans@gmail.com>
2020-03-03 16:46:08 +0000

    Adding section 7 midterm exam - started

---
+++ Section_7_Midterm_Exam/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Section_7_Midterm_Exam")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section_7_Midterm_Exam")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c939683c-cb66-4e26-aa58-5fecbfb9663b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Section_7_Midterm_Exam/Receipt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_7_Midterm_Exam
+{
+    class Receipt
+    {
+        private int receiptNumber;
+        private string dateOfPurchase;
+        private int customerNumber;
+        private string customerFirstName;
+        private string customerLastName;
+        private string customerAddress;
+        private string customerPhoneNUmber;
+        private int itemNumber;
+        private string Description;
+        private decimal unitPrice;
+        private int quantityPurchased;
+
+        // constructor
+        public Receipt(int receiptNum, string dateOfPurch, int customerNum, string custFirstName, string custLastName, string custAddress, string custPhone, int itemNum, string Desc, decimal Price, int quantity)
+        {
+            receiptNum = ReceiptNumber;
+        }
+
+        public int ReceiptNumber
+        {
+            get
+            {
+                return receiptNumber;
+            }
+            set
+            {
+                if (receiptNumber > 0)
+                {
+                    receiptNumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("incorrect value entered please enter > zero number");
+                }
+            }
+        }
+            
+
+
+
+
+    }
+    }
