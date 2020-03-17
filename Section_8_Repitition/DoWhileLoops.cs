b169c206992b22d9539addbf0cbe0b98a70ef5e4
ukgarethevans@gmail.com <ukgarethevans@gmail.com>
2020-03-17 09:30:39 +0000

    Adding ForLoops and DoWhileLoops to section 8

---
+++ Section_8_Repitition/DoWhileLoops.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section_8_Repitition
+{
+
+    /*
+     * Lesson 91 Do while loops
+     * Do while is a post test wheras a for loop is a pre test.
+     * 
+     * 
+     * 
+     */
+    [TestClass]
+    public class DoWhileLoop
+    {
+        [TestMethod]
+        public void TestDoWhile()
+        {
+            int sum = 0;
+            int num = 1;
+            do
+            {
+                sum += num; // sum = sum + num
+                num++;
+                Console.WriteLine($"sum = {sum}");
+            }
+            while (num<=10);
+            Assert.AreEqual(55, sum);
+
+        }
+    }
+}
---
+++ Section_8_Repitition/ForLoops.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section_8_Repitition
+{
+
+    // Lesson 90 For Loops
+    // x += y same as  x = x + y  
+
+    [TestClass]
+    public class ForLoops
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int inner = 0;
+            // for (init;test;update)
+            for (int outer = 0; outer < 3; outer++)
+            {
+                // inner loop 
+                for(inner=10; inner > 5; inner--)
+                {
+                    Console.WriteLine($"Outer: {outer}\tInner: {inner}");
+                }
+
+            }
+        }
+        [TestMethod]
+        public void StrangeForLoops()
+        {
+            // initialise 2 variables
+            for(int counter = 0,val1 = 10;counter < val1; counter++)   // not recomended
+            {
+            }
+            // can use floats as well
+            for(double d=15.0;d<20.0; d += 0.5) 
+            {
+
+            }
+
+        }
+    }
+}
